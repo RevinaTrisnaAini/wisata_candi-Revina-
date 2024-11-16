f3a3460672f8d8fc0881521a449ccc46debfb1d4
RevinaTrisnaAini <revinatrisnaaini_2226240102@mhs.mdp.ac.id>
2024-11-16 09:19:32 +0700

    Navigasi dan Routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/screen/favorite_screen.dart';
+import 'package:wisata_candi/screen/home_screen.dart';
+import 'package:wisata_candi/screen/profile_screen.dart';
 //import 'package:wisata_candi/screen/home_screen.dart';
 import 'package:wisata_candi/screen/search_screen.dart';
+import 'package:wisata_candi/screen/sign_in_screen.dart';
+import 'package:wisata_candi/screen/sign_up_screen.dart';
 //import 'package:wisata_candi/screen/detail_screen.dart';
 //import 'data/candi_data.dart';
 // import 'package:wisata_candi/data/candi_data.dart';
@@ -8,6 +13,7 @@ import 'package:wisata_candi/screen/search_screen.dart';
 //import 'package:wisata_candi/screen/sign_in_screen.dart';
 //import 'package:wisata_candi/screen/sign_up_screen.dart';
 
+// Fungsi main yang mejalankan Aplikasi
 void main() {
   runApp(const MyApp());
 }
@@ -15,11 +21,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'wisata candi',
+      title: 'Wisata Candi',
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
           iconTheme: IconThemeData(color: Colors.deepPurple),
@@ -27,8 +32,8 @@ class MyApp extends StatelessWidget {
             color: Colors.deepPurple,
             fontSize: 20,
             fontWeight: FontWeight.bold,
-          )
-      ),
+          ),
+        ),
         colorScheme:
         ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
           primary: Colors.deepPurple,
@@ -36,12 +41,94 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      //home: HomeScreen(),
-      home: SearchScreen(),
-      //home: SignUpScreen(),
-      //home: SignInScreen(),
-       //home: ProfileScreen(),
-      //home: DetailScreen(candi: candiList[0]),
+      home: MainScreen(),
+      initialRoute: '/',
+      routes: {
+        '/homescreen': (context) => const HomeScreen(),
+        '/signin': (context) => SignInScreen(),
+        '/signup': (context) => SignUpScreen(),
+      },
+
+      // Pertemuan 10-20
+      // home: const ProfileScreen(),
+      // home: const DetailScreen(candi: candiList[0]),
+      // home: const SignInScreen(),
+      // home: const SearchScreen(),
+      // home: const HomeScreen(),
+      // home: const SignUpScreen(),
+    );
+  }
+}
+
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  // TODO: 1. Deklarasikan variabel
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomeScreen(),
+    SearchScreen(),
+    FavoriteScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // TODO: 2. Buat properti body berupa widget yang ditampilkan
+      body: _children[_currentIndex],
+      // TODO: 3. Buat properti bottomNavigationBar dengan nilai Theme
+      bottomNavigationBar: Theme(
+        // TODO: 4. Buat data dan Child dari Theme
+        data: Theme.of(context).copyWith(canvasColor: Colors.deepPurple[50]),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Colors.deepPurple,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: Colors.deepPurple,
+              ),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.deepPurple,
+              ),
+              label: 'Favorite',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: Colors.deepPurple,
+              ),
+              label: 'Person',
+            ),
+          ],
+          selectedItemColor: Colors.deepPurple,
+          unselectedItemColor: Colors.deepPurple[100],
+          showSelectedLabels: true,
+        ),
+      ),
     );
   }
 }
